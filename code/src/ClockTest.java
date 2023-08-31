b0f2ab5ab81e6cce43aa941cfbc0a3d5d647bb79
Rafael Parreira Chequer <73084877+RafaelPChequer@users.noreply.github.com>
2023-08-30 23:35:40 -0300

    Update ClockTest.java adding Comments

--- code/src/ClockTest.java
+++ code/src/ClockTest.java
@@ -6,50 +6,50 @@ public class ClockTest {
     @Test
     public void testSetAndGetHours() {
         Clock clock = new Clock();
-        clock.set(10, 30, 45);
-        assertEquals(10, clock.getHours());
+        clock.set(10, 30, 45); // Define o horário do relógio
+        assertEquals(10, clock.getHours()); // Verifica se o valor das horas está correto
     }
 
     @Test
     public void testSetInvalidTime() {
         Clock clock = new Clock();
-        clock.set(25, 30, 45); // Invalid hours
-        assertEquals(0, clock.getHours()); // Hours should not change
+        clock.set(25, 30, 45); // Tenta definir um horário inválido
+        assertEquals(0, clock.getHours()); // Verifica se as horas não foram alteradas
     }
 
     @Test
     public void testSetAndGetMinutes() {
         Clock clock = new Clock();
-        clock.set(10, 30, 45);
-        assertEquals(30, clock.getMinutes());
+        clock.set(10, 30, 45); // Define o horário do relógio
+        assertEquals(30, clock.getMinutes()); // Verifica se o valor dos minutos está correto
     }
 
     @Test
     public void testSetInvalidMinutes() {
         Clock clock = new Clock();
-        clock.set(10, 65, 45); // Invalid minutes
-        assertEquals(0, clock.getMinutes()); // Minutes should not change
+        clock.set(10, 65, 45); // Tenta definir minutos inválidos
+        assertEquals(0, clock.getMinutes()); // Verifica se os minutos não foram alterados
     }
 
     @Test
     public void testSetAndGetSeconds() {
         Clock clock = new Clock();
-        clock.set(10, 30, 45);
-        assertEquals(45, clock.getSeconds());
+        clock.set(10, 30, 45); // Define o horário do relógio
+        assertEquals(45, clock.getSeconds()); // Verifica se o valor dos segundos está correto
     }
 
     @Test
     public void testSetInvalidSeconds() {
         Clock clock = new Clock();
-        clock.set(10, 30, 75); // Invalid seconds
-        assertEquals(0, clock.getSeconds()); // Seconds should not change
+        clock.set(10, 30, 75); // Tenta definir segundos inválidos
+        assertEquals(0, clock.getSeconds()); // Verifica se os segundos não foram alterados
     }
 
     @Test
     public void testReset() {
         Clock clock = new Clock();
         clock.set(10, 30, 45);
-        clock.reset();
+        clock.reset(); // Reseta o relógio
         assertEquals(0, clock.getHours());
         assertEquals(0, clock.getMinutes());
         assertEquals(0, clock.getSeconds());
@@ -59,28 +59,28 @@ public class ClockTest {
     public void testPrint24HourFormat() {
         Clock clock = new Clock();
         clock.set(15, 20, 30);
-        clock.print24HourFormat();
+        clock.print24HourFormat(); // Imprime o horário no formato de 24 horas
     }
 
     @Test
     public void testPrintAMPMFormat() {
         Clock clock = new Clock();
         clock.set(8, 45, 0);
-        clock.printAMPMFormat();
+        clock.printAMPMFormat(); // Imprime o horário no formato AM/PM
     }
 
     @Test
     public void testStopWatch() {
         Clock clock = new Clock();
         clock.set(10, 0, 0);
-        int elapsed = clock.stopWatch(10, 15, 30);
-        assertEquals(930, elapsed);
+        int elapsed = clock.stopWatch(10, 15, 30); // Calcula o tempo decorrido
+        assertEquals(930, elapsed); // Verifica se o tempo decorrido está correto
     }
 
     @Test
     public void testStopWatchInvalidTime() {
         Clock clock = new Clock();
-        int elapsed = clock.stopWatch(10, 65, 30); // Invalid time
-        assertEquals(-1, elapsed); // Indicates an error
+        int elapsed = clock.stopWatch(10, 65, 30); // Tenta calcular tempo com horário inválido
+        assertEquals(-1, elapsed); // Verifica se o resultado indica erro
     }
 }
